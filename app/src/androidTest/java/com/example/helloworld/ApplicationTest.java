2cd6592bbd1929b8b54b677e2945ecf85ae4f196
Bill Mote (Contractor) <bmote@exacttarget.com>
2015-01-02 08:58:55 -0500

    Started working on unit tests.

--- app/src/androidTest/java/com/example/helloworld/ApplicationTest.java
+++ app/src/androidTest/java/com/example/helloworld/ApplicationTest.java
@@ -1,13 +1,20 @@
 package com.example.helloworld;
 
-import android.app.Application;
 import android.test.ApplicationTestCase;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
+public class ApplicationTest extends ApplicationTestCase<HelloWorldApplication> {
     public ApplicationTest() {
-        super(Application.class);
+        super(HelloWorldApplication.class);
     }
+
+    public void testGCMIdSet() {
+        assertEquals(getApplication().ANALYTICS_ENABLED, false);
+        assertEquals(getApplication().LOCATION_ENABLED, false);
+        assertEquals(getApplication().CLOUD_PAGES_ENABLED, false);
+    }
+
 }
+
