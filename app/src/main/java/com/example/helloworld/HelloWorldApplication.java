b35ec5f472bce470eb8bb3c6273e3e75919ca428
Bill Mote (Contractor) <bmote@exacttarget.com>
2015-01-15 11:04:06 -0500

    Code cleanup and UI work.

--- app/src/main/java/com/example/helloworld/HelloWorldApplication.java
+++ app/src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -5,6 +5,7 @@ import android.app.Application;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.text.format.DateUtils;
@@ -25,10 +26,13 @@ public class HelloWorldApplication extends Application {
 
     // Enabling location here also triggers work in our Activity that must be done.
     public static final boolean LOCATION_ENABLED = false;
-    public static final boolean ANALYTICS_ENABLED = false;
+    public static final boolean ANALYTICS_ENABLED = true;
     public static final boolean CLOUD_PAGES_ENABLED = false;
     public static final long MIDDLE_TIER_PROPAGATION_MIN_DELAY = DateUtils.MINUTE_IN_MILLIS * 15; // 15 min.
     public static final String EXTRAS_REGISTRATION_EVENT = "event";
+    public static final String HELLO_WORLD_PREFERENCES = "hello_world_preferences";
+    public static final String KEY_PREFS_ALARM_TIME = "mt_alarm_time";
+    public static final String INTENT_ACTION_STRING = "mt_propagation_alarm";
 
     public static String VERSION_NAME;
     public static int VERSION_CODE;
@@ -49,32 +53,27 @@ public class HelloWorldApplication extends Application {
         EventBus.getDefault().register(this);
 
         try {
-
             // Set the log level based on the build type.
             ETPush.setLogLevel(BuildConfig.DEBUG ? Log.VERBOSE : Log.ERROR);
 
-            // Register to recieve push notifications.
+            // Register to receive push notifications.
             ETPush.readyAimFire(
                     this,
-                    getString(R.string.et_app_id),      // TODO Replace with Your App Center Application ID
-                    getString(R.string.access_token),   // TODO Replace with Your App Center Access Token
-                    getString(R.string.gcm_sender_id),  // TODO Replace with Your GCM Sender ID
+                    getString(R.string.et_app_id),   // TODO Replace with Your App Center Application ID
+                    getString(R.string.access_token),               // TODO Replace with Your App Center Access Token
+                    getString(R.string.gcm_sender_id),                           // TODO Replace with Your GCM Sender ID
                     ANALYTICS_ENABLED,
                     LOCATION_ENABLED,
                     CLOUD_PAGES_ENABLED
             );
-
             /*
                 A good practice is to add the application's version name as a tag that can later
                 be used to target push notifications to specific application versions.
              */
             ETPush pushManager = ETPush.pushManager();
             pushManager.addTag(VERSION_NAME);
-
         } catch (ETException e) {
-
             Log.e(TAG, e.getMessage(), e);
-
         }
     }
 
@@ -85,17 +84,11 @@ public class HelloWorldApplication extends Application {
      * @return a String representing the application version name
      */
     private String getAppVersionName() {
-
         String developerBuild = BuildConfig.DEBUG ? "d" : "";
-
         try {
-
             return getPackageManager().getPackageInfo(getPackageName(), 0).versionName + developerBuild;
-
         } catch (PackageManager.NameNotFoundException e) {
-
             throw new RuntimeException(e.getMessage());
-
         }
     }
 
@@ -105,15 +98,10 @@ public class HelloWorldApplication extends Application {
      * @return an int representing the application version code
      */
     private int getAppVersionCode() {
-
         try {
-
             return this.getPackageManager().getPackageInfo(getPackageName(), 0).versionCode;
-
         } catch (PackageManager.NameNotFoundException e) {
-
             throw new RuntimeException(e.getMessage());
-
         }
     }
 
@@ -125,16 +113,13 @@ public class HelloWorldApplication extends Application {
     public void onEvent(final RegistrationEvent event) {
 
         if (ETPush.getLogLevel() <= Log.DEBUG) {
-
             Log.d(TAG, "Marketing Cloud update occurred.");
             Log.d(TAG, "Device ID:" + event.getDeviceId());
             Log.d(TAG, "Device Token:" + event.getDeviceToken());
             Log.d(TAG, "Subscriber key:" + event.getSubscriberKey());
-
             for (Attribute attribute : event.getAttributes()) {
                 Log.d(TAG, "Attribute " + attribute.getKey() + ": [" + attribute.getValue() + "]");
             }
-
             Log.d(TAG, "Tags: " + event.getTags());
             Log.d(TAG, "Language: " + event.getLocale());
             Log.d(TAG, String.format("Last sent: %1$d", System.currentTimeMillis()));
@@ -169,7 +154,7 @@ public class HelloWorldApplication extends Application {
 
         okToCheckMiddleTier = proposedCheckTime;
         Log.v(TAG, String.format("Setting an alarm for %3$dms from %1$d (alarm time: %2$d)", System.currentTimeMillis(), okToCheckMiddleTier, okToCheckMiddleTier - System.currentTimeMillis()));
-        Intent intent = new Intent("mt_propagation_alarm");
+        Intent intent = new Intent(INTENT_ACTION_STRING);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         Bundle bundle = new Bundle();
         bundle.putSerializable(EXTRAS_REGISTRATION_EVENT, event);
@@ -186,5 +171,8 @@ public class HelloWorldApplication extends Application {
                 okToCheckMiddleTier,
                 pendingIntent
         );
+
+        SharedPreferences.Editor preferencesEditor = getSharedPreferences(HELLO_WORLD_PREFERENCES, MODE_PRIVATE).edit();
+        preferencesEditor.putLong(KEY_PREFS_ALARM_TIME, okToCheckMiddleTier).apply();
     }
 }
--- app/src/main/java/com/example/helloworld/ui/activities/MainActivity.java
+++ app/src/main/java/com/example/helloworld/ui/activities/MainActivity.java
@@ -6,7 +6,13 @@ import android.bluetooth.BluetoothAdapter;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.format.DateUtils;
 import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import com.exacttarget.etpushsdk.ETException;
 import com.exacttarget.etpushsdk.ETLocationManager;
@@ -16,20 +22,109 @@ import com.example.helloworld.R;
 import com.radiusnetworks.ibeacon.BleNotAvailableException;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     private static final String TAG = MainActivity.class.getSimpleName();
     private static final String KEY_FIRST_LAUNCH = "first_launch";
-
+    private static final String KEY_PREFS_PUSH_ENABLED = "push_enabled";
+    private static final String KEY_PREFS_WATCHING_LOCATION = "watching_location";
+    private final Runnable displayTimeRemainingRunnable = new Runnable() {
+        @Override
+        public void run() {
+            displayTimeRemaining();
+        }
+    };
     private SharedPreferences sharedPreferences;
     private SharedPreferences.Editor preferencesEditor;
+    private ToggleButton toggleButtonEnablePush;
+    private ToggleButton toggleButtonEnableLocation;
+    private TextView countDownTimer;
+    private boolean isPushEnabled;
+    private long alarmTime;
+    private boolean isWatchingLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        sharedPreferences = getPreferences(MODE_PRIVATE);
+
+        sharedPreferences = getSharedPreferences(HelloWorldApplication.HELLO_WORLD_PREFERENCES, MODE_PRIVATE);
         preferencesEditor = sharedPreferences.edit();
+
+        /*
+            How long until any changes here are reflected in the Marketing Cloud?
+         */
+        alarmTime = sharedPreferences.getLong(HelloWorldApplication.KEY_PREFS_ALARM_TIME, 0);
+
+        /*
+            Keep track of the last user pushEnabled() state.
+         */
+        isPushEnabled = sharedPreferences.getBoolean(KEY_PREFS_PUSH_ENABLED, true);
+        isWatchingLocation = sharedPreferences.getBoolean(KEY_PREFS_WATCHING_LOCATION, false);
+
+        /*
+            Our countdown timer view.  Shows the seconds until the middle tier updates are
+            propagated to the Marketing Cloud servers.
+         */
+        countDownTimer = (TextView) findViewById(R.id.tv_countdown_timer);
+
+        /*
+            Our pushEnabled() toggle button.  Set its state based off the preferences and create
+            a clicklistener.  Be sure to update the user's selected state and store it.
+         */
+        toggleButtonEnablePush = (ToggleButton) findViewById(R.id.toggle_enablePush);
+        toggleButtonEnablePush.setChecked(isPushEnabled);
+        toggleButtonEnablePush.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toggleButtonEnablePush.toggle();
+                isPushEnabled = !isPushEnabled;
+                try {
+                    if (isPushEnabled) {
+                        Log.i(TAG, "Enabling push.");
+                        ETPush.pushManager().enablePush();
+                    } else {
+                        Log.i(TAG, "Disabling push.");
+                        ETPush.pushManager().disablePush();
+                    }
+                    ((ToggleButton) v).setChecked(isPushEnabled);
+                    preferencesEditor.putBoolean(KEY_PREFS_PUSH_ENABLED, isPushEnabled).apply();
+                } catch (ETException e) {
+                    Log.e(TAG, e.getMessage(), e);
+                }
+            }
+        });
+
+        /*
+            Only display the LOCATION toggle if Location is enabled in readyAimFire();
+         */
+        if (HelloWorldApplication.LOCATION_ENABLED) {
+            LinearLayout locationLayout = (LinearLayout) findViewById(R.id.layout_location);
+            locationLayout.setVisibility(View.VISIBLE);
+            toggleButtonEnableLocation = (ToggleButton) findViewById(R.id.toggle_enableLocation);
+            toggleButtonEnableLocation.setChecked(isWatchingLocation);
+            toggleButtonEnableLocation.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    toggleButtonEnableLocation.toggle();
+                    isWatchingLocation = !isWatchingLocation;
+                    try {
+                        if (isWatchingLocation) {
+                            Log.i(TAG, "Watching location.");
+                            ETLocationManager.locationManager().startWatchingLocation();
+                        } else {
+                            Log.i(TAG, "Not watching location.");
+                            ETLocationManager.locationManager().stopWatchingLocation();
+                        }
+                        ((ToggleButton) v).setChecked(isWatchingLocation);
+                        preferencesEditor.putBoolean(KEY_PREFS_WATCHING_LOCATION, isWatchingLocation).apply();
+                    } catch (ETException e) {
+                        Log.e(TAG, e.getMessage(), e);
+                    }
+                }
+            });
+        }
+
         try {
             /*
                 We must call enablePush() at least once for the application.
@@ -45,6 +140,7 @@ public class MainActivity extends Activity {
                 preferencesEditor.putBoolean(KEY_FIRST_LAUNCH, false).apply();
                 Log.i(TAG, String.format("Updated %1$s to false", KEY_FIRST_LAUNCH));
             }
+
             /*
                 If we have Location enabled then we must start/stop watching as the default state
                 at least once.
@@ -66,6 +162,7 @@ public class MainActivity extends Activity {
                     ETLocationManager.locationManager().stopWatchingProximity();
                 }
             }
+
             /*
                 Add First & Last Name Attributes & a Subscriber Key
              */
@@ -74,20 +171,73 @@ public class MainActivity extends Activity {
             ETPush.pushManager().addAttribute("LastName", getString(R.string.gcm_sender_id));
             Log.i(TAG, "Adding subscriber key.");
             ETPush.pushManager().setSubscriberKey("bmote@exacttarget.com");
-            Log.i(TAG, "Enabling push.");
-            ETPush.pushManager().enablePush();
+
+            if (isPushEnabled) {
+                Log.i(TAG, "Push is enabled.");
+                ETPush.pushManager().enablePush();
+            } else {
+                Log.i(TAG, "Push is disabled.");
+                ETPush.pushManager().disablePush();
+            }
+
         } catch (ETException e) {
             Log.e(TAG, e.getMessage(), e);
         }
     }
 
+    /*
+        Calculate the number of seconds remaining and update our view.  Also, call our runnable
+        1 sec. from now which will return us here ;)
+     */
+    private void displayTimeRemaining() {
+        long millisecondsRemaining = alarmTime - System.currentTimeMillis();
+        int secondsRemaining = (int) (millisecondsRemaining / DateUtils.SECOND_IN_MILLIS);
+        if (secondsRemaining > 0) {
+            countDownTimer.setText(String.format(getString(R.string.countdown_timer_text), secondsRemaining, getResources().getQuantityString(R.plurals.seconds, secondsRemaining)));
+            countDownTimer.postDelayed(displayTimeRemainingRunnable, DateUtils.SECOND_IN_MILLIS);
+        } else {
+            countDownTimer.setText(getString(R.string.no_pending_alarms));
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        /*
+            This is only for the displaying of our countdown timer and is not required by the
+            JB4A SDK.
+         */
+        sharedPreferences.registerOnSharedPreferenceChangeListener(this);
+        this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        displayTimeRemaining();
+    }
+
+    @Override
+    protected void onPause() {
+        /*
+            Tidy up.
+         */
+        countDownTimer.removeCallbacks(displayTimeRemainingRunnable);
+        /*
+            This is only for the displaying of our countdown timer and is not required by the
+            JB4A SDK.
+         */
+        sharedPreferences.unregisterOnSharedPreferenceChangeListener(this);
+        this.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        super.onPause();
+    }
+
+    /**
+     * Prompt the user to enable BlueTooth on their device if it is not already enabled and update
+     * their LocationManager object to startWatchingProximity().
+     */
     private void promptForBluetoothSettings() {
         new AlertDialog.Builder(this)
                 .setIcon(android.R.drawable.ic_dialog_info)
-                .setTitle("Enable Bluetooth?")
-                .setMessage("Beacon alerts require that you have Bluetooth enabled. Enable it now?")
-                .setNegativeButton("Cancel", null)
-                .setPositiveButton("Enable Bluetooth", new DialogInterface.OnClickListener() {
+                .setTitle(getString(R.string.dialog_enable_bluetooth_title))
+                .setMessage(getString(R.string.dialog_enable_bluetooth_message))
+                .setNegativeButton(getString(R.string.btn_cancel), null)
+                .setPositiveButton(getString(R.string.btn_enable_bluetooth), new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
@@ -105,4 +255,16 @@ public class MainActivity extends Activity {
                 }).show();
     }
 
+    /*
+        Our onEvent() EventBus callback is in our Application Class.  It will update our alarm time
+        via SharedPreferences so implement the onSharedPreferenceChanged() interface to reflect the
+        changes in our view.
+     */
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (key.equals(HelloWorldApplication.KEY_PREFS_ALARM_TIME)) {
+            alarmTime = sharedPreferences.getLong(HelloWorldApplication.KEY_PREFS_ALARM_TIME, 0);
+            displayTimeRemaining();
+        }
+    }
 }
